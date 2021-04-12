bccaa97e7fff6c66f57cf79ba52bd83ea43c6fe9
cthacker-udel <cthacker@udel>
2021-04-12 03:56:17 -0400

    Added boolean branch to handle guessing part of game

--- numberGuessGame.sh
+++ numberGuessGame.sh
@@ -5,7 +5,35 @@ echo -e "\nWelcome to the Bash Number Guess Game!\n"
 
 echo -e "\n--------------------------------------\n"
 
+echo -e "\n RULES : Random number will be generated each round, the computer will guess a number, and you will guess a number, whoever guesses the correct number will score a point, depending on your point differential between the computer and you, you will be awared a medal\n"
 
-randomNumber=$RANDOM
 
-echo "$randomnumber"
+yourscore=0
+computerscore=0
+
+read -p "\n Guess a random number between x and y, type in x : >>> " x
+read -p "\n Now Type in y : >>>  " y
+
+loopCondition=1
+
+
+while [ $loopCondition -eq 1 ] ; do
+    echo -e "Guessing number --- "
+    sleep 1
+    randomNumber=$((x + $RANDOM % y))
+    computerGuess=$((x + $RANDOM % y))
+    read -p "\nYour guess?\n" your_guess
+    if [ $your_guess -eq $randomNumber -a $computerGuess -eq $randomNumber ]; then
+        ((yourscore++))
+        ((computerscore++))
+        echo -e "The random number was $randomNumber\n"
+    elif [ $your_guess -eq $randomNumber -a $computerGuess -ne $randomNumber ]; then
+        ((yourscore++))
+        echo -e "The random number was $randomnumber\n"
+    elif [ $your_guess -ne $randomNumber -a $computerGuess -eq $randomNumber ]; then
+        ((computerscore++))
+        echo -e "\nThe random number was $randomnumber\n"
+    else
+        echo "\nThe random number was $randomNumber\n"
+    fi
+done
